e05aed8f71cb7f0a803e50cbd85076ada649ef44
Prashant Kumar <prashantkumar.pacific@gmail.com>
2017-11-21 17:05:10 +0530

    Commit changes

--- src/main/java/com/springboot/customerAPI/customerapi/customer/Customer.java
+++ src/main/java/com/springboot/customerAPI/customerapi/customer/Customer.java
@@ -9,11 +9,12 @@ public class Customer {
 	private Integer pincode;
 	private String birthdate;
 	private boolean isActive = true;
-	
-	
-	public Customer(){}
-	
-	public Customer(String id, String fName, String lName, String email, Integer pincode, String birthdate) {
+
+	public Customer() {
+	}
+
+	public Customer(String id, String fName, String lName, String email,
+			Integer pincode, String birthdate) {
 		super();
 		this.id = id;
 		this.fName = fName;
@@ -23,39 +24,51 @@ public class Customer {
 		this.birthdate = birthdate;
 		this.isActive = true;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getfName() {
 		return fName;
 	}
+
 	public void setfName(String fName) {
 		this.fName = fName;
 	}
+
 	public String getlName() {
 		return lName;
 	}
+
 	public void setlName(String lName) {
 		this.lName = lName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Integer getPincode() {
 		return pincode;
 	}
+
 	public void setPincode(Integer pincode) {
 		this.pincode = pincode;
 	}
+
 	public String getBirthdate() {
 		return birthdate;
 	}
+
 	public void setBirthdate(String birthdate) {
 		this.birthdate = birthdate;
 	}
@@ -67,41 +80,18 @@ public class Customer {
 	public void setActive(boolean isActive) {
 		this.isActive = isActive;
 	}
-	
 
-public boolean equals(Object object) {
-	if (this == object)
-		return true;
-	if (object == null)
-		return false;
-	if (getClass() != object.getClass())
-		return false;
-	Customer c = (Customer) object;
+	public boolean equals(Object object) {
+		if (this == object)
+			return true;
+		if (object == null)
+			return false;
+		if (getClass() != object.getClass())
+			return false;
+		Customer c = (Customer) object;
 
-	//return this.getId() ==c.getId()? true : false;
-	
-	return c.getId().equals(this.getId())? true : false;
-	
-	/*if (c.getId() == this.getId()) 
-		return true;
-	  if (!(c.getId().equals(this.getId())))
-		return false;
-	*/
-	
-	
-	
-    
-}
-/*public boolean equals(Object obj) {
-    if (this == obj)
-        return true;
-    if (obj == null)
-        return false;
-    if (!(obj instanceof Emp))
-        return false;
-    Emp other = (Emp) obj;
-    return code == null ? other.code == null : code.equals(other.code);
-}
-*/	
-	
+		return c.getId().equals(this.getId()) ? true : false;
+
+	}
+
 }
